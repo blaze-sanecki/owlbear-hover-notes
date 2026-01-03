2f424c0fbf6f42b60781d1eacb6574052cb46eed
Blaze Sanecki <sanecki.blazej@gmail.com>
2026-01-03 19:56:59 +0100

    Initial defaults implementation

--- src/contextMenu.ts
+++ src/contextMenu.ts
@@ -1,4 +1,5 @@
 import OBR from "@owlbear-rodeo/sdk";
+import type { Image } from "@owlbear-rodeo/sdk";
 import { ID } from "./constants";
 
 export function setupContextMenu() {
@@ -80,6 +81,9 @@ export function setupContextMenu() {
 				icon: "/images/icon-disable.svg",
 				label: "Remove Hover Note",
 				filter: {
+					every: [
+						{ key: ["metadata", `${ID}/note`], value: undefined, operator: "!=" }
+					],
 					roles: ["GM"],
 				},
 			},
@@ -99,6 +103,33 @@ export function setupContextMenu() {
 			await OBR.player.deselect();
 			if (deleted) {
 				OBR.notification.show("Hover note removed.", "WARNING");
+			}
+		},
+	});
+
+	OBR.contextMenu.create({
+		id: `${ID}/context-menu-make-default`,
+		icons: [
+			{
+				icon: "/images/icon-default.svg",
+				label: "Make Hover Default",
+				filter: {
+					every: [
+						{ key: ["metadata", `${ID}/note`], value: undefined, operator: "!=" },
+						{ key: "type", value: "IMAGE" },
+					],
+				},
+			},
+		],
+		async onClick(context) {
+			const item = context.items[0] as Image;
+			const metadata = item.metadata[`${ID}/note`] as { url: string };
+			if (metadata && metadata.url && item.image && item.image.url) {
+				const defaults = JSON.parse(localStorage.getItem(`${ID}/defaults`) || "{}");
+				defaults[item.image.url] = metadata.url;
+				localStorage.setItem(`${ID}/defaults`, JSON.stringify(defaults));
+				await OBR.notification.show("Default hover note set.", "INFO");
+				await OBR.player.deselect();
 			}
 		},
 	});
